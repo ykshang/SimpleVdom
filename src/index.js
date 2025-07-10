73222dc246a54f9dd3e91930f74d508e41a6abed
kangkang shang <535634238@qq.com>
2025-07-10 20:17:56 +0800

    准备相同节点精细比较的数据

--- src/index.js
+++ src/index.js
@@ -2,34 +2,21 @@ import render from "./diy/render.js";
 import patch from "./diy/patch.js";
 const container = document.getElementById("container");
 
-const vnode1 = render("ul", {}, [
-  render("li", {}, "1"),
-  render("li", {}, "2"),
-  render("li", {}, [
-    render("div", {}, [
-      render("ol", {}, [
-        render("li", {}, "A"),
-        render("li", {}, "B"),
-        render("li", {}, "C"),
-      ]),
-    ]),
-  ]),
-  render("li", {}, "4"),
+const vnode1 = render("section", {}, [
+  render("p", {}, "A"),
+  render("p", {}, "B"),
+  render("p", {}, "C"),
 ]);
-
-console.log(vnode1);
 patch(container, vnode1);
 
+console.log("vnode1", vnode1);
+
 const vnode2 = render("section", {}, [
-  render("h1", {}, "你好"),
-  render("p", {}, "这是一个段落"),
-  render("ul", {}, [
-    render("li", {}, "列表项1"),
-    render("li", {}, "列表项2"),
-    render("li", {}, "列表项3"),
-  ]),
+  render("p", {}, "A"),
+  render("p", {}, "B"),
+  render("p", {}, "C"),
+  render("p", {}, "D"),
 ]);
-console.log(vnode2);
 
 // 点击按钮 将Vnode1 变更为 vnode2
 const btn = document.getElementById("btn");
