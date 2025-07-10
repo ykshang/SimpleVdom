0d5c66efb3ae57c550d270c94b4f2a707fc15e69
kangkang shang <535634238@qq.com>
2025-07-10 15:50:36 +0800

    添加jsdoc

--- src/diy/vnode.js
+++ src/diy/vnode.js
@@ -1,3 +1,12 @@
+/**
+ *
+ * @param {*} selector 选择器
+ * @param {*} data 数据
+ * @param {*} children 子节点
+ * @param {*} text 文本
+ * @param {*} element 对应的 DOM 元素对象
+ * @returns
+ */
 export default function (selector, data, children, text, element) {
   return {
     selector,
