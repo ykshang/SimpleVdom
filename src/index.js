f75f785da7ae5bb86a5b860d34d0741fe7b621e0
kangkang shang <535634238@qq.com>
2025-07-10 19:39:07 +0800

    修改按钮方法，第二次patch

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import render from "./diy/render.js";
 import patch from "./diy/patch.js";
 const container = document.getElementById("container");
 
-const vnode1 = render("h1", {}, "你好");
-const vnode2 = render("ul", {}, [
+const vnode1 = render("ul", {}, [
   render("li", {}, "1"),
   render("li", {}, "2"),
   render("li", {}, [
@@ -19,11 +18,21 @@ const vnode2 = render("ul", {}, [
 ]);
 
 console.log(vnode1);
+patch(container, vnode1);
+
+const vnode2 = render("section", {}, [
+  render("h1", {}, "你好"),
+  render("p", {}, "这是一个段落"),
+  render("ul", {}, [
+    render("li", {}, "列表项1"),
+    render("li", {}, "列表项2"),
+    render("li", {}, "列表项3"),
+  ]),
+]);
 console.log(vnode2);
-// patch(container, vnode2);
 
 // 点击按钮 将Vnode1 变更为 vnode2
 const btn = document.getElementById("btn");
 btn.onclick = function () {
-  patch(container, vnode2);
+  patch(vnode1, vnode2);
 };
