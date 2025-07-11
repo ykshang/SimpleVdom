95f69699272eea293058cbe76bd4cece5dd14e0b
kangkang shang <535634238@qq.com>
2025-07-11 19:00:08 +0800

    修改注释

--- src/diy/vnode.js
+++ src/diy/vnode.js
@@ -4,7 +4,7 @@
  * @param {*} data 数据
  * @param {*} children 子节点
  * @param {*} text 文本
- * @param {*} element 对应的 DOM 元素对象
+ * @param {*} element 对应的 DOM 元素对象，他的值是实时获取
  * @returns
  */
 export default function (selector, data, children, text, element) {
